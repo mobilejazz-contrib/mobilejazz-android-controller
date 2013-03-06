b7dc84ad55b2a52f75f2126a172662505c14d7ff
Hannes Widmoser <widmoser@gmail.com>
2013-03-06 09:48:28 +0100

    Generalized implementation of a categorised adapter to support any kind of delegate adapter

--- src/cat/mobilejazz/controller/adapter/CategorizedAdapter.java
+++ src/cat/mobilejazz/controller/adapter/CategorizedAdapter.java
@@ -3,10 +3,11 @@ package cat.mobilejazz.controller.adapter;
 import java.util.TreeMap;
 
 import android.content.Context;
-import android.database.Cursor;
-import android.support.v4.widget.CursorAdapter;
+import android.database.DataSetObserver;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
 
 /**
  * This class implements an adapter that allows to visually order it's elements
@@ -21,27 +22,26 @@ import android.view.ViewGroup;
  *            that it need not store all sub elements, as these are stored in
  *            the corresponding cursor.
  */
-public abstract class CategorizedCursorAdapter<Category> extends CursorAdapter {
+public abstract class CategorizedAdapter<Category, AdapterType extends Adapter> extends BaseAdapter {
 
 	public static final int VIEW_TYPE_ITEM = 0;
 	public static final int VIEW_TYPE_CATEGORY_HEADER = 1;
 
+	private Context mContext;
+	private AdapterType mDelegate;
 	private TreeMap<Integer, Category> mHeaderIndices;
 
-	private void initialize(Cursor c) {
+	private void initialize(AdapterType delegate) {
 		mHeaderIndices = new TreeMap<Integer, Category>();
 		mHeaderIndices.clear();
-		recomputeHeaderIndices(mHeaderIndices, c);
+		recomputeHeaderIndices(mHeaderIndices, delegate);
 	}
 
-	public CategorizedCursorAdapter(Context context, Cursor c, int flags) {
-		super(context, c, flags);
-		initialize(c);
-	}
-
-	public CategorizedCursorAdapter(Context context, Cursor c, boolean autoRequery) {
-		super(context, c, autoRequery);
-		initialize(c);
+	public CategorizedAdapter(Context context, AdapterType delegate) {
+		mContext = context;
+		mDelegate = delegate;
+		mDelegate.registerDataSetObserver(new HeaderUpdater());
+		initialize(delegate);
 	}
 
 	/**
@@ -58,22 +58,15 @@ public abstract class CategorizedCursorAdapter<Category> extends CursorAdapter {
 	 * @param cursor
 	 *            The new cursor.
 	 */
-	protected abstract void recomputeHeaderIndices(TreeMap<Integer, Category> headerIndices, Cursor cursor);
+	protected abstract void recomputeHeaderIndices(TreeMap<Integer, Category> headerIndices, AdapterType delegate);
 
 	private int getCursorIndex(int position) {
 		return position - mHeaderIndices.headMap(position).size();
 	}
 
 	@Override
-	public Cursor swapCursor(Cursor newCursor) {
-		mHeaderIndices.clear();
-		recomputeHeaderIndices(mHeaderIndices, newCursor);
-		return super.swapCursor(newCursor);
-	}
-
-	@Override
 	public int getCount() {
-		return super.getCount() + mHeaderIndices.size();
+		return mDelegate.getCount() + mHeaderIndices.size();
 	}
 
 	@Override
@@ -86,7 +79,7 @@ public abstract class CategorizedCursorAdapter<Category> extends CursorAdapter {
 			// this position points to a cursor element. Compute the cursor
 			// index:
 
-			return super.getItem(getCursorIndex(position));
+			return mDelegate.getItem(getCursorIndex(position));
 		}
 	}
 
@@ -99,7 +92,7 @@ public abstract class CategorizedCursorAdapter<Category> extends CursorAdapter {
 		} else {
 			// this position points to a cursor element. Compute the cursor
 			// index:
-			return super.getItemId(getCursorIndex(position));
+			return mDelegate.getItemId(getCursorIndex(position));
 		}
 	}
 
@@ -117,26 +110,48 @@ public abstract class CategorizedCursorAdapter<Category> extends CursorAdapter {
 			return VIEW_TYPE_ITEM;
 		}
 	}
-	
+
 	public abstract View newView(Context context, Category category, ViewGroup parent);
-	
+
 	public abstract void bindView(View view, Context context, Category category);
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		Category header = mHeaderIndices.get(position);
 		if (header != null) {
 			View v;
-	        if (convertView == null) {
-	            v = newView(mContext, header, parent);
-	        } else {
-	            v = convertView;
-	        }
-	        bindView(v, mContext, header);
-	        return v;
+			if (convertView == null) {
+				v = newView(mContext, header, parent);
+			} else {
+				v = convertView;
+			}
+			bindView(v, mContext, header);
+			return v;
 		} else {
-			return super.getView(getCursorIndex(position), convertView, parent);
+			return mDelegate.getView(getCursorIndex(position), convertView, parent);
 		}
 	}
 
+	public AdapterType getDelegate() {
+		return mDelegate;
+	}
+
+	private class HeaderUpdater extends DataSetObserver {
+
+		@Override
+		public void onChanged() {
+			super.onChanged();
+			mHeaderIndices.clear();
+			recomputeHeaderIndices(mHeaderIndices, mDelegate);
+
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public void onInvalidated() {
+			super.onInvalidated();
+			notifyDataSetInvalidated();
+		}
+
+	}
 }
